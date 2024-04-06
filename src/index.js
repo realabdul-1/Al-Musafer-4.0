54a09d1d3e1b2035a7344401bb65104dbeb7e620
Abdulrahman Aljaadi <110558473+realabdul-1@users.noreply.github.com>
2024-04-06 07:32:46 +0300

    update5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '/src/index.js'
 import './index.css';
 import RouterDOM from './Router';
 
@@ -7,6 +8,6 @@ import RouterDOM from './Router';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterDOM />
+    <RouterDOM/>
   </React.StrictMode>
 );
